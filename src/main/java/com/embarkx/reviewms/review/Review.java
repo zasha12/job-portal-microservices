97d00cf76321fc243a4435a121e2be572c1be273
zasha12 <114907005+zasha12@users.noreply.github.com>
2026-08-24 13:04:28 +0530

    Rename package from com.embarkx to com.zasha12

--- src/main/java/com/embarkx/reviewms/review/Review.java
+++ src/main/java/com/embarkx/reviewms/review/Review.java
@@ -1,4 +1,4 @@
-package com.embarkx.reviewms.review;
+package com.zasha12.reviewms.review;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;

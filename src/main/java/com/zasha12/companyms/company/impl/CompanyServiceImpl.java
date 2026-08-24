69db9016d02879a83c6f214f278df06e6310a885
zasha12 <114907005+zasha12@users.noreply.github.com>
2026-08-24 13:37:51 +0530

    Move CompanyServiceImpl from com.embarkx to com.zasha12 package structure

---
+++ src/main/java/com/zasha12/companyms/company/impl/CompanyServiceImpl.java
@@ -0,0 +1,60 @@
+package com.zasha12.companyms.company.impl;
+import com.zasha12.companyms.company.Company;
+import com.zasha12.companyms.company.CompanyRepository;
+import com.zasha12.companyms.company.CompanyService;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CompanyServiceImpl implements CompanyService {
+    private CompanyRepository companyRepository;
+
+    public CompanyServiceImpl(CompanyRepository companyRepository) {
+        this.companyRepository = companyRepository;
+    }
+
+    @Override
+    public List<Company> getAllCompanies() {
+        return companyRepository.findAll();
+    }
+
+    @Override
+    public boolean updateCompany(Company company, Long id) {
+        Optional<Company> companyOptional = companyRepository.findById(id);
+        if (companyOptional.isPresent()) {
+            //we are NOT updating the id here, as it is a unique identifier which is Not needs to be updated...
+            Company companyToUpdate = companyOptional.get();
+            companyToUpdate.setDescription(company.getDescription());
+            companyToUpdate.setName(company.getName());
+
+            companyRepository.save(companyToUpdate);
+            return true;
+        } else {
+
+            return false;
+        }
+
+    }
+
+    @Override
+    public void createCompany(Company company) {
+        companyRepository.save(company);
+    }
+
+    @Override
+    public boolean deleteCompanyById(Long id) {
+        if (companyRepository.existsById(id)) {
+            companyRepository.deleteById(id);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public Company getCompanyById(Long id) {
+        return companyRepository.findById(id).orElse(null);
+
+    }
+}

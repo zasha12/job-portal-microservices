1756b18f2a0158530e31baee6a36a7ad62809f11
zasha12 <114907005+zasha12@users.noreply.github.com>
2026-08-24 13:07:09 +0530

    Change package name from com.embarkx to com.zasha12

--- src/main/java/com/embarkx/reviewms/ReviewmsApplication.java
+++ src/main/java/com/embarkx/reviewms/ReviewmsApplication.java
@@ -1,4 +1,4 @@
-package com.embarkx.reviewms;
+package com.zasha12.reviewms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
